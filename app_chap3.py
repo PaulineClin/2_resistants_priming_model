ba5f2084c86978eb2044f82b3b728391950fcba5
PaulineClin <94557380+PaulineClin@users.noreply.github.com>
2022-06-02 18:34:59 +0200

    Add files via upload
    Retrait du model fullpriming

--- app_chap3.py
+++ app_chap3.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Custom imports 
 from multipage import MultiPage
-from pages import presentation, Simu_primingless, Simu_fullpriming, Simu_general_model
+from pages import presentation, Simu_primingless, Simu_general_model
 
 # Create an instance of the app 
 app = MultiPage()
@@ -13,7 +13,6 @@ st.title("")
 # Add all your applications (pages) here
 app.add_page("Presentation", presentation.app)
 app.add_page("Primingless model", Simu_primingless.app)
-app.add_page("Fullpriming model", Simu_fullpriming.app)
 app.add_page("General model", Simu_general_model.app)
 
 # The main app
